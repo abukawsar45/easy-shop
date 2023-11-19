94897d1e8b580c51d343e35cba9260a62ea6d29f
abukawsar45 <kawsar1146236@gmail.com>
2023-11-19 23:15:46 +0600

    handle destroy erroer

--- src/app/login/LoginForm.jsx
+++ src/app/login/LoginForm.jsx
@@ -1,6 +1,7 @@
 'use client'
 
 import useAuth from '@/hooks/useAuth';
+import Link from 'next/link';
 import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react';
 import { useForm } from 'react-hook-form';
@@ -26,8 +27,7 @@ const LoginForm = () => {
 
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='card-body' >
-      
+    <form onSubmit={handleSubmit(onSubmit)} className='card-body' >      
       <div className="form-control">
         <label htmlFor="email" className='label label-text'>
           Email
@@ -48,6 +48,37 @@ const LoginForm = () => {
           <span className='text-red-500 text-base mt-3' >Please enter a valid email address.</span>
         ) }
       </div>
+      <div className="form-control">
+        <label htmlFor="password" className='label label-text'>
+          Password
+        </label>
+        <input
+        type='password'
+        placeholder='password'
+        id='password'
+        name='password'
+          className='input input-bordered'
+          autoComplete='password'
+          { ...register("password",  {
+            required: true,
+            minLength: 8
+          })}
+        />
+        { errors.password && (
+          <span className='text-red-500 text-base mt-3' >Please enter a password.</span>
+        ) }
+        <label>
+          <a href="#" className='label-text-alt link link-hover' >Forget password?</a>
+        </label>
+      </div>
+      <p className='mt-3' >
+        Don&apos; t have an accoutn? 
+        <Link href='/signup'
+          className='text-blue-500 under
+           underline ml-1'
+        >Signup</Link>
+
+      </p>
     </form>
   );
 };
